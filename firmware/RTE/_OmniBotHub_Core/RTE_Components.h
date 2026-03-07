e1fb0fd09f6d77c7adf6def210a4b3fbcda67aa7
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2026-03-07 19:57:16 +0300

    Misc: удалил ненужный файл

--- firmware/RTE/_OmniBotHub_Core/RTE_Components.h
+++ firmware/RTE/_OmniBotHub_Core/RTE_Components.h
@@ -10,12 +10,6 @@
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
 
-
-/*
- * Define the Device Header File: 
- */
-#define CMSIS_device_header "stm32f4xx.h"
-
 /* Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
 #define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
           #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
