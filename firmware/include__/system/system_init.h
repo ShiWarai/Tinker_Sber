21486a39c0d687aefb6d7217186f4c93d8787916
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2026-03-14 21:01:29 +0300

    Build: убрал библиотеку и сделал её пакетной

--- firmware/include__/system/system_init.h
+++
@@ -1,6 +0,0 @@
-#ifndef SYSTEM_INIT_H
-#define SYSTEM_INIT_H
-
-void SystemClock_Config(void);
-
-#endif /* SYSTEM_INIT_H */
--- firmware/src__/app/app_main_data.c
+++
@@ -1,9 +0,0 @@
-#include "app_main.h"
-
-_MEMS mems;
-VMC_ALL vmc_all;
-
-_LEG_MOTOR leg_motor;
-motor_measure_t motor_chassis[NUM_MOTORS];
-
-// Data storage for MEMS and VMC structures
